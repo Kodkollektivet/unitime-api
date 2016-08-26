d627dd3d31c860d091a9d2eb3abfa54ff565f529
jherrlin <jherrlin@gmail.com>
2016-08-26 11:41:08 +0200

    Basic stuff for unitime added.

--- unitime/settings/settings.py
+++ unitime/settings/settings.py
@@ -133,3 +133,79 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.BrowsableAPIRenderer',
     )
 }
+
+# Logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+        },
+        'verbose': {
+            'format': '[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s',
+            'datefmt': '%d/%b/%Y %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'default': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': 'logs/unitime.log',
+            'maxBytes': 1024 * 1024 * 5,  # 5BM
+            'backupCount': 5,
+            'formatter': 'standard',
+        },
+        'django': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': 'logs/django.log',
+            'maxBytes': 1024 * 1024 * 5,  # 5MB
+            'backupCount': 5,
+            'formatter': 'standard'
+        },
+        'timeedit_lnu_api': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': 'logs/timeedit_lnu_api.log',
+            'maxBytes': 1024 * 1024 * 5,  # 5MB
+            'backupCount': 5,
+            'formatter': 'verbose'
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'unitime': {
+            'handlers': ['default', 'console'],
+            'level': 'DEBUG',
+            'propagate': True
+        },
+        'django': {
+            'handlers': ['django'],
+            'level': 'DEBUG',
+            'propagate': True
+        },
+        'django.requests': {
+            'handlers': ['django'],
+            'level': 'DEBUG',
+            'propagate': True
+        },
+        'django.template': {
+            'handlers': ['django'],
+            'level': 'DEBUG',
+            'propagate': True
+        },
+        'timeedit_lnu_api': {
+            'handlers': ['timeedit_lnu_api', 'console'],
+            'level': 'DEBUG',
+            'propagate': True
+        },
+    }
+}
--- unitime/unitime/views.py
+++ unitime/unitime/views.py
@@ -1,3 +1,71 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from timeedit_lnu_api import get_course
+
+from unitime.models import Course
+from unitime.forms import CourseForm
+from unitime.serializers import CourseSerializer
+
+from unitime import exceptions
+
+
+KODKOLLEKTIVET_HEADER = {'Provided-By': 'kodkollektivet.se'}  # In header respons
+
+
+class CourseListView(APIView):
+
+    def get(self, request):
+        courses = []
+        for course in Course.objects.all():
+            if course not in courses:
+                courses.append(course)
+        course_serializer = CourseSerializer(courses, many=True)
+
+        return Response(course_serializer.data,
+                        headers=KODKOLLEKTIVET_HEADER,
+                        status=status.HTTP_200_OK)
+
+    def head(self, request):
+        headers = {'Content-Length': len(Course.objects.all()), 'Provided-By': 'kodkollektivet.se'}
+        return Response(
+            headers=headers,
+            status=status.HTTP_200_OK)
+
+
+class CourseView(APIView):
+
+    def get(self, request, code_in):
+
+        form = CourseForm({'course': code_in})
+
+        if form.is_valid():
+            code = form.cleaned_data['course'].upper()
+
+            #save_course_code(code)  # Save code for future use
+
+            # Does course already exists in DB
+            if Course.objects.filter(course_code__exact=code).exists():
+                course = Course.objects.filter(course_code__iexact=code).latest(field_name='modified')
+                return Response(
+                    CourseSerializer(course).data,
+                    headers=KODKOLLEKTIVET_HEADER,
+                    status=status.HTTP_200_OK)
+
+            else:
+
+                found_course = get_course(code)
+
+                if found_course:
+                    found_course = found_course[0]
+                    course = Course.objects.filter(course_code__iexact=code).latest(field_name='modified')
+                    return Response(
+                        CourseSerializer(course).data,
+                        headers=KODKOLLEKTIVET_HEADER,
+                        status=status.HTTP_200_OK)
+
+            return exceptions.can_find_course(code_in)
+
+        else:
+            return exceptions.invalid_search_format()
