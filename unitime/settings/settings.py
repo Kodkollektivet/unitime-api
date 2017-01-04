28fc04bd72b87477f0c62742cf42937cbee1f0bc
jherrlin <jherrlin@gmail.com>
2017-01-04 12:57:30 +0100

    Static files paths fix.

--- unitime/settings/settings.py
+++ unitime/settings/settings.py
@@ -25,6 +25,7 @@ SECRET_KEY = 'c-mp&e5g1xz$#74&0$18rx$+ert1m81)rqm0uzolhut2ats0ij'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+
 ALLOWED_HOSTS = ["*"]
 
 
@@ -126,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "../static")
 
 
 REST_FRAMEWORK = {
